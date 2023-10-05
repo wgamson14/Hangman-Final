9e86779a5807e69c96fcd79151f0ba9a8f58d57b
Will Gamson <willgamson@Wills-Air.Home>
2023-10-05 13:36:35 +0100

    docstrings

--- milestone_5.py
+++ milestone_5.py
@@ -2,6 +2,16 @@ import random
 
 class Hangman:
     def __init__(self, word_list, num_lives = 5):
+        """
+        The function initializes a Hangman game with a given word list and number of lives, and sets up
+        the initial state of the game.
+        
+        :param word_list: The `word_list` parameter is a list of words from which the computer will
+        randomly choose a word for the game. Each word in the list should be a string
+        :param num_lives: The `num_lives` parameter is used to specify the number of lives or guesses
+        the player has in the game. By default, it is set to 5 if no value is provided when creating an
+        instance of the class, defaults to 5 (optional)
+        """
         self.word_list = word_list
         self.num_lives = num_lives
         self.computer_choice = random.choice(self.word_list)
@@ -27,6 +37,10 @@ class Hangman:
             
 
     def ask_for_input(self):
+        """
+        The function asks the user for input, checks if it is a valid letter, and then checks if the letter
+        has already been guessed.
+        """
         guess = input('Please enter a letter: ')
         if guess.isalpha() == False or len(guess) > 1:
             print('Invalid letter. Please, enter a single alphabetical character.')
@@ -39,6 +53,17 @@ class Hangman:
 
 
 def play_game(word_list):
+    """
+    The function `play_game` plays a game of Hangman using a given word list until the player either
+    wins or loses.
+
+    -------------------------------------------------------------------------------------------------
+
+    parameters: 
+    - word_list: The `word_list` parameter is a list of words that will be used in the Hangman
+    game. Each word in the list represents a possible word that the player needs to guess
+
+    """
     game = Hangman(word_list, num_lives = 5)
     while True:
         if game.num_lives == 0:
